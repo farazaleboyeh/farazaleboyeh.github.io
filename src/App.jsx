c97a8e9f93149d8a92cc73d6ffab2b36fd7807f4
Faraz.Aleboyeh <faraz.aleboyeh@forces.gc.ca>
2025-12-13 16:21:21 -0500

    Repo setup & header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import TitleBar from './components/TitleBar.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <TitleBar/>
+      {/* <p>Text</p> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TitleBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TitleBar(){
+    return(
+        <header>
+            <h1>Yancheng Qiu</h1>
+            <nav>
+              <a href="hey">gallery</a>
+              <a href="hey">for fun</a>
+              <a href="hey">about</a>
+            </nav>
+        </header>
+    )
+}
+
+export default TitleBar
