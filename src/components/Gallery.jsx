44bf081d5b9cb9f3c0f4c64e34f91e8a95e23585
Faraz.Aleboyeh <faraz.aleboyeh@forces.gc.ca>
2026-01-22 11:14:52 -0500

    image fetching added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import {testImages} from '../data/photos.js';
 
+//Load all images (immediately, if eager is false they're loaded lazily)
+const images = import.meta.glob('../assets/McMaster MBB/*.{png,jpg,jpeg,webp}', { eager: true });
+
 function Gallery(){
-  const sortedImages = [...testImages].sort(
-    (a, b) => a.order - b.order
-  );
+  // const sortedImages = [...testImages].sort(
+  //   (a, b) => a.order - b.order
+  // );
+
+  const imageList = Object.values(images).map((img) => img.default);
 
   return (
-    <div className="gallery-grid" id="gallery">
-      {sortedImages.map(image => (
-        <img
-          key={image.id}
-          src={image.src}
-          alt={image.alt}
-          loading="lazy"
-        />
+     <div className="gallery-grid" id="gallery">
+      {imageList.map((imgSrc, index) => (
+        <img key={index} src={imgSrc} alt={`Portfolio shot ${index}`} />
       ))}
-      
-     
-    
-    </div>
+     </div>
   )
 }
 
 export default Gallery
+
+//   {sortedImages.map(image => (
+//     <img
+//       key={image.id}
+//       src={image.src}
+//       alt={image.alt}
+//       loading="lazy"
+//     />
+//   ))}
